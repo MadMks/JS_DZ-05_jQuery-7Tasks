ea98287ee96c5aad54a9057fbf67c16edf7199f3
Makc <mks59k@gmail.com>
2019-01-03 23:46:49 +0200

    Реализовао задание 1

--- pages/task1/js/main.js
+++ pages/task1/js/main.js
@@ -1,7 +1,18 @@
 /* Взаимодействия с элементами */
 
-$('h2, p') // Первый, второй, третий и т.д.
-$('h2 span') // outer и inner 
-$('h2 > span') // parent > child 
-$('h2 + p') // prev + next 
+// Перечислению (первый, второй, третий...)
+$('h2, li').css('font-weight', 'bold'); // Первый, второй, третий и т.д.
+
+// Найти все span внутри блока .container (элемент span - потомок)
+$('.container span').css('border', '1px solid green'); // outer и inner 
+
+// Найти только те span, которые находятся 
+// НЕПОСРЕДСТВЕННО внутри .container (span - дочерний элемент)
+$('.container > span').css('color', 'orange'); // parent > child 
+
+// Найти только ближайший сестринский элемент для тега H2
+$('h2 + p').css('background-color', '#ff0000'); // prev + next
+
+// Найти все сестринские элементы для тега H2
 $('h2 ~ p').css('border', '2px solid #000'); // prev ~ next 	
+$('h2 ~ p').css('padding', '10px'); // prev ~ next 	
