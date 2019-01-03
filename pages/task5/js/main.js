d196ae6630803e308f9eeb062b1d1208959e8c4e
Makc <mks59k@gmail.com>
2019-01-04 00:35:39 +0200

    Реализовано задание 5

--- pages/task5/js/main.js
+++ pages/task5/js/main.js
@@ -2,17 +2,10 @@
 
 /* Фильтры форм в jQuery */		
 
-// $('input').css('border', '1px solid red'); // Все поля input 
-// $('input:button').css('border', '1px solid red'); // Поле button - простая кнопка
-// $('input:radio').css('display', 'block'); // Радио-кнопки 
-// $('input:checkbox').css('display', 'block'); // Чекбоксы
-// $('input:text').css('border', '1px solid red');// Текстовые поля для ввода
-// $('input:password').css('border', '1px solid red'); // Поле для ввода пароля
-// $('input:file').css('border', '1px solid red'); // Поле для загрузки файла
-// $('input:submit').css('border', '1px solid red'); // Кнопка "Отправить"
-// $('input:reset').css('border', '1px solid red'); // Кнопка "Сбросить"
-// $('input:image').css('border', '1px solid red'); // Кнопка-картинка 
-// $('input:focus').css('display', 'block'); // Выбранное поле (состояние фокус)
-// $('input:checked').css('border', '1px solid red'); // Выбранный чекбокс или радио-кнопка
-// $('input:enabled').css('border', '1px solid red'); // Доступное поле
-// $('input:disabled').css('border', '1px solid red'); // Недоступное поле
+$('.main-form .form-control').css('border', '2px solid green'); // Все элементы формы с классом form-control
+
+$('input:button, input:submit, input:reset')
+.css('border', '2px solid red')
+.css('background-color', 'yellow'); // Все кнопки
+
+$('input:radio[name="gender"]').css('margin-left', '100px'); // Радио-кнопки 
